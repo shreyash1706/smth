7016f247f3d96be09cdd7b51506a3ebc739f5d82
adeetee-k <aditikharat315@gmail.com>
2024-04-01 16:05:37 +0530

    profile page improved

--- to_do_list/lib/pages/profile.dart
+++ to_do_list/lib/pages/profile.dart
@@ -3,160 +3,54 @@ import 'package:flutter/material.dart';
 class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(70),
-        child: AppBar(
-          backgroundColor:  Color.fromARGB(255,18,26,38),
-          elevation: 0.0,
-          flexibleSpace: Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: GestureDetector(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(110),
+          child: AppBar(
+            backgroundColor: Color.fromARGB(255, 18, 35, 60),
+            elevation: 0,
+            flexibleSpace: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    GestureDetector(
                       onTap: () {},
                       child: Padding(
-                        padding: EdgeInsets.all(16.0),
+                        padding: EdgeInsets.only(
+                            left: 18, top: 40, bottom: 38, right: 18),
                         child: CircleAvatar(
-                          backgroundColor:  Colors.white,
-                          radius: 30,
-                          child: Icon(
-                            Icons.person,
-                            color: Colors.black,
-                            size: 40,
-                          ),
+                          backgroundColor: Colors.white,
+                          radius: 33,
+                          backgroundImage: NetworkImage(
+                              'https://icon-library.com/images/default-user-icon/default-user-icon-23.jpg'),
                         ),
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'User 123',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'User 123',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 19,
+                            fontFamily: 'Lato',
+                          ),
                         ),
-                        
-                      ),
-                      MouseRegion(
-                        cursor: SystemMouseCursors.click,
-                        child: GestureDetector(
+                        GestureDetector(
                           onTap: () {},
                           child: Text(
-                            'User profile',
+                            'meow meow id',
                             style: TextStyle(
                               color: Colors.white,
                               fontSize: 14,
+                              fontFamily: 'Lato',
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: ProfileSection(),
-    );
-  }
-}
-
-class ProfileSection extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var _mediaQuery = MediaQuery.of(context).size;
-    int completedTasks =
-        0; // Example: Replace with actual completed tasks count
-    int pendingTasks = 0; // Example: Replace with actual pending tasks count
-
-    return Row(
-      children: [
-        SizedBox(
-          width: _mediaQuery.width * 0.05,
-        ),
-        Container(
-          height: _mediaQuery.height,
-          width: _mediaQuery.width * 0.9,
-          // color: Colors.yellow,
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: _mediaQuery.height * 0.025,
-                ),
-                Container(
-                  child: Text(
-                    'Tasks Overview', // New Container for "Tasks Overview"
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: _mediaQuery.height * 0.025,
-                ),
-                Row(
-                  children: <Widget>[
-                    Card(
-                      elevation: 10,
-                      child: Container(
-                        width: _mediaQuery.width * 0.42,
-                        height: 80,//_mediaQuery.height * 0.15,
-                        decoration: BoxDecoration(
-                          color: Colors.blue.shade100,
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            Text(
-                              'Completed Tasks',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            Text(
-                              '$completedTasks', // Display completed tasks count
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: _mediaQuery.width * 0.015,
-                      height: _mediaQuery.height * 0.15,
-                    ),
-                    Card(
-                      elevation: 10,
-                      child: Container(
-                        width: _mediaQuery.width * 0.42,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.blue.shade100,
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            Text(
-                              'Pending Tasks',
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            Text(
-                              '$pendingTasks', // Display completed tasks count
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ),
+                      ],
                     ),
                   ],
                 ),
@@ -164,10 +58,97 @@ class ProfileSection extends StatelessWidget {
             ),
           ),
         ),
-        SizedBox(
-          width: _mediaQuery.width * 0.05,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 15),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Text(
+                  'Tasks Overview',
+                  style: TextStyle(fontSize: 20, fontFamily: 'Lato'),
+                ),
+              ),
+            ),
+            SizedBox(height: 5),
+            
+            Expanded(
+              child: ListView(
+                children: [
+                  buildCategoryRow(context, ['Current Task Streak', 1, Icons.trending_up]),
+                  buildCategoryRow(context, ['Maximum Task Streak', 2, Icons.leaderboard]),
+                  buildCategoryRow(context, ['Completed Tasks', 3, Icons.check_circle]),
+                  buildCategoryRow(context, ['Pending Task', 4, Icons.access_time]),
+                ],
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
+    );
+  }
+
+  Widget buildCategoryRow(BuildContext context, List<dynamic> categoryData) {
+    String categoryName = categoryData[0];
+    int integerValue = categoryData[1];
+    IconData iconData = categoryData[2];
+
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          vertical: 2, horizontal: 10),
+      child: Card(
+        elevation: 5, 
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Container(
+          padding: EdgeInsets.symmetric(
+              vertical: 15, horizontal: 15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            gradient: LinearGradient(
+              colors: [Color.fromARGB(255, 54, 68, 152), Color.fromARGB(255, 59, 149, 223)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(iconData, color: Colors.white),
+                  SizedBox(width: 10),
+                
+                  Text(
+                    categoryName,
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10), 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(width: 34),
+                  Text(
+                    '0',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                  Text(
+                    '$integerValue',
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
